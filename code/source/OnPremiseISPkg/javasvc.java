52ff397646d9052330463b9b10c965e08e09056c
rahuldemocenter <rahs@softwareag.com>
2023-07-17 12:53:32 +0530

    first commit.

---
+++ code/source/OnPremiseISPkg/javasvc.java
@@ -0,0 +1,38 @@
+package OnPremiseISPkg;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class javasvc
+
+{
+	// ---( internal utility methods )---
+
+	final static javasvc _instance = new javasvc();
+
+	static javasvc _newInstance() { return new javasvc(); }
+
+	static javasvc _cast(Object o) { return (javasvc)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void calculateUUID (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(calculateUUID)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
